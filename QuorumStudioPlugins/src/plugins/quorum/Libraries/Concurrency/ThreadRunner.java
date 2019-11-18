5079576fb214892572acfe9ec35f1c62895f4d05
Bill Allee <wallee777@hotmail.com>
2019-11-18 13:32:47 -0800

    Made the Stop button stop all running programs, regardless of whether or not the program was being debugged.

--- QuorumStudioPlugins/src/plugins/quorum/Libraries/Concurrency/ThreadRunner.java
+++ QuorumStudioPlugins/src/plugins/quorum/Libraries/Concurrency/ThreadRunner.java
@@ -5,10 +5,11 @@
  */
 package plugins.quorum.Libraries.Concurrency;
 
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  *
@@ -17,12 +18,21 @@ import java.util.concurrent.ThreadPoolExecutor;
 public class ThreadRunner {
     public java.lang.Object me_ = null;
     ExecutorService executor = Executors.newSingleThreadExecutor();
-    
+    LinkedList<Future<?>> futures = new LinkedList<>();
     public void Add(quorum.Libraries.Interface.Behaviors.Behavior_ action) {
         RunnableBehavior act = new RunnableBehavior(action);
         Future<?> submit = executor.submit(act);
+        futures.add(submit);
     }
     
+    public void Empty() {
+        try {
+        Iterator<Future<?>> iterator = futures.iterator(); 
+        while(iterator.hasNext()) {
+            Future<?> future = iterator.next();
+            boolean cancel = future.cancel(true);
+        }}catch(Exception e) {}//ignore it, if you have to.
+    }
     public void ShutDown() {
         executor.shutdown();
     }
